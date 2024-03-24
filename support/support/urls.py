0b04d0918aa3f08eedb3a3a28101f129df91790d
Oleksandr Radchenko <xras89@gmail.com>
2024-03-24 13:31:59 +0100

    Django start. Exchange rates application - Done V1

--- support/support/urls.py
+++ support/support/urls.py
@@ -12,23 +12,20 @@ create_random_string: Callable[[int], str] = lambda size: "".join(  # noqa
     [random.choice(string.ascii_letters) for i in range(size)]  # noqa
 )  # noqa
 
+
 def generate_article_idea(request: HttpRequest) -> JsonResponse:
     content = {
         "title": create_random_string(size=10),
         "description": create_random_string(size=20),
     }
-    return JsonResponse(
-        content
-    )
+    return JsonResponse(content)
 
 
 async def get_current_market_state(request: HttpRequest):
     url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=XSN17SDSA5RAM5W2"
     async with httpx.AsyncClient() as client:
         response: httpx.Response = await client.get(url)
-    rate: str = response.json()["Realtime Currency Exchange Rate"][
-        "5. Exchange Rate"
-    ]
+    rate: str = response.json()["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
     return JsonResponse({"rate": rate})
 
 
@@ -41,9 +38,7 @@ async def get_exchange_rate(request: HttpRequest) -> HttpResponse:
 
     async with httpx.AsyncClient() as client:
         response: httpx.Response = await client.get(url=url)
-    rate: str = response.json()["Realtime Currency Exchange Rate"][
-        "5. Exchange Rate"
-    ]
+    rate: str = response.json()["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
 
     result = f"Exchange rate for {source} to {destination} is {rate}"
     return HttpResponse(result)
